52d7550cbd7670e9e568c327be30d4d1c8093d78
JulianChu <yulang.chu@gmail.com>
2017-01-17 18:06:35 +0800

    Scenario 3: buy Potter#1x1, #2x1,#3x1 , pay 270 dollar Add: -CalculateSalePriceTest_Scenario3_Potter1x1_Potter2x1_Potter3x1_Returns_270()--failed

--- PotterShoppingCartTests/PotterShoppingCartTests.cs
+++ PotterShoppingCartTests/PotterShoppingCartTests.cs
@@ -37,6 +37,21 @@ namespace PotterBookStoreTests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
+        public void CalculateSalePriceTest_Scenario3_Potter1x1_Potter2x1_Potter3x1_Returns_270()
+        {
+            //Assign
+            var books = GetBooks(1, 1, 1, 0, 0);
+            var shoppingCart = new PotterShoppingCart();
+
+            //Act
+            var actual = shoppingCart.CalculateSalePrice(books);
+
+            //Assert
+            var expected = 190;
+            Assert.AreEqual(expected, actual);
+        }
+
         private IEnumerable<Book> GetBooks(int Potter1, int Potter2, int Potter3, int Potter4, int Potter5)
         {
             return new List<Book> {
