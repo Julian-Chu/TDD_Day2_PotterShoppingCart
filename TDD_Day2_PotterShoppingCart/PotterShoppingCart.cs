56af19f360a1ac42756aae231e2f739eec285be2
JulianChu <yulang.chu@gmail.com>
2017-01-17 23:24:36 +0800

    Introduce Business Logic:  Calculate multiple sets of discount Add: - Business Logic: calculate multiple sets of discount Other: - CalculateSalePriceTest_Scenario7_Potter1x1_Potter2x2_Potter3x2_Returns_460()-- passed

--- TDD_Day2_PotterShoppingCart/PotterShoppingCart.cs
+++ TDD_Day2_PotterShoppingCart/PotterShoppingCart.cs
@@ -23,18 +23,32 @@ namespace PotterBookStore
         {
             double pricePerBook = 100;
             int oneSet = 1;
+            int howManyBooksInASet = 0;
+            double salePriceOfASet = 0;
+            
 
             int countOfBooks = books.Sum(book => book.count);
             var countsOfEachBook = books.Select(book => book.count);
+            int booksCountNoDiscount = countOfBooks;
 
-            int howManyBooksInASet = countsOfEachBook.Where(BookCountMoreThanZero()).Count();
-            double salePriceOfASet = howManyBooksInASet * oneSet * pricePerBook * _discountByHowManyBooksInASet[howManyBooksInASet];
+            while (IsThereAnySet(countsOfEachBook))
+            {
+                howManyBooksInASet = countsOfEachBook.Where(BookCountMoreThanZero()).Count();
+                salePriceOfASet += howManyBooksInASet * oneSet * pricePerBook * _discountByHowManyBooksInASet[howManyBooksInASet];
+                countsOfEachBook = countsOfEachBook.Select(bookCount => bookCount > 0 ? bookCount - 1 : 0);
+                booksCountNoDiscount -= howManyBooksInASet;
+            }
 
-            int booksCountNoDiscount = countOfBooks - howManyBooksInASet;
+            
             double salePriceofNoSet = booksCountNoDiscount * pricePerBook;
 
             double salePrice = salePriceOfASet + salePriceofNoSet;
             return salePrice;
+        }
+
+        private static bool IsThereAnySet(IEnumerable<int> countsOfEachBook)
+        {
+            return countsOfEachBook.Where(BookCountMoreThanZero()).Count() > 1;
         }
 
         private static System.Func<int, bool> BookCountMoreThanZero()
