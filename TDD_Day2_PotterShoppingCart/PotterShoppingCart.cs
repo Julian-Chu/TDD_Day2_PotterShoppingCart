ab589d5fb7d1aa9b27840cec226badbbc660ff23
JulianChu <yulang.chu@gmail.com>
2017-01-17 18:56:33 +0800

    Use dictionary to replace GetDiscountByCount(int) Add: - dictionary  _discountByCountOfBooks<countOfBooks, discount> and initialize in constructor. Remove: - GetDiscountByCount(int countOfBooks)

--- TDD_Day2_PotterShoppingCart/PotterShoppingCart.cs
+++ TDD_Day2_PotterShoppingCart/PotterShoppingCart.cs
@@ -1,40 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace PotterBookStore
 {
     public class PotterShoppingCart
     {
+        private Dictionary<int, double> _discountByCountOfBooks;
+
         public PotterShoppingCart()
         {
+            _discountByCountOfBooks = new Dictionary<int, double>()
+            {
+                { 1,1 },
+                { 2,0.95 },
+                { 3,0.9 },
+                { 4,0.8 },
+                { 5,0.75 }
+            };
         }
 
         public double CalculateSalePrice(IEnumerable<Book> books)
         {
             double pricePerBook = 100;
             int countOfBooks = books.Select(book => book.count).Sum();
-            double discount = 0;
-            discount = GetDiscountByCount(countOfBooks);
+            double discount = 1;
+            discount = _discountByCountOfBooks[countOfBooks];
 
             double salePrice = countOfBooks * pricePerBook * discount;
             return salePrice;
         }
 
-        private static double GetDiscountByCount(int countOfBooks)
-        {
-            double discount = 0;
-            if (countOfBooks == 1)
-                discount = 1;
-            if (countOfBooks == 2)
-                discount = 0.95;
-            if (countOfBooks == 3)
-                discount = 0.9;
-            if (countOfBooks == 4)
-                discount = 0.8;
-            if (countOfBooks == 5)
-                discount = 0.75;
-            return discount;
-        }
     }
 }
